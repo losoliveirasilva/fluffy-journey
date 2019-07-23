5c44c4e4a553738441ffd206f18e6e00d8a4988d
Leonardo de Oliveira da Silva <los.oliveirasilva@gmail.com>
2019-07-22 21:29:03 -0300

    add user model and remove erd pdf

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_140745) do
+ActiveRecord::Schema.define(version: 2019_07_23_002449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 2019_07_22_140745) do
     t.bigint "prerequisite_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["code"], name: "index_users_on_code", unique: true
   end
 
 end
